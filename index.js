597bddf79f83ae156b37948891d713e11fcba74f
sac6inarya <sachinkarya333@gmail.com>
2023-08-11 20:32:51 +0530

    solid half diamond star

---
+++ index.js
@@ -0,0 +1,15 @@
+for(let i=1; i<=5;i++){
+    let str = "";
+    for(let j = 1; j<=i; j++){
+        str+="*";
+    }
+    console.log(str);
+}
+
+for(let i=1 ; i<=4; i++){
+    let str ="";
+    for(let j = 1; j<=4-i+1; j++){
+        str+="*";
+    }
+    console.log(str);
+}
