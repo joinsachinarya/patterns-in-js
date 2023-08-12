403e6042ce031a6d7f202c54750903a30bc93b70
sac6inarya <sachinkarya333@gmail.com>
2023-08-12 16:04:52 +0530

    hollow-diamond-star

---
+++ index.js
@@ -0,0 +1,22 @@
+for(let i =1; i<=5; i++){
+    let str='';
+    for(let j = 1; j<=9; j++){
+        if(j==6-i || j==4+i){
+            str+="*";
+        }else{
+            str+=' ';
+        }
+    }
+    console.log(str);
+}
+for(let i =1; i<=5; i++){
+    let str='';
+    for(let j = 1; j<=9; j++){
+        if(j==i || j==10-i){
+            str+="*";
+        }else{
+            str+=' ';
+        }
+    }
+    console.log(str);
+}
